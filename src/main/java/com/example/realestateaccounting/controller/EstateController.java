2afa1233695c89c92d1bdb0aed55b4371ef99387
strangecodder <strangecodder@gmail.com>
2023-04-08 21:34:37 +0300

    Добавлено html страницы а так-же реализованы контроллеры

--- src/main/java/com/example/realestateaccounting/controller/EstateController.java
+++ src/main/java/com/example/realestateaccounting/controller/EstateController.java
@@ -1,13 +1,21 @@
 package com.example.realestateaccounting.controller;
 
+import com.example.realestateaccounting.dto.EstateDto;
 import com.example.realestateaccounting.dto.mapper.EstateMapper;
+import com.example.realestateaccounting.model.Estate;
+import com.example.realestateaccounting.model.Work;
 import com.example.realestateaccounting.service.EstateService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.mapstruct.Mapper;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/estate")
@@ -16,5 +24,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class EstateController {
     EstateService estateService;
     EstateMapper estateMapper;
+
+
+    @GetMapping("/estate-index")
+    public String getEstateIndex(@RequestParam("workList") List<Work> workList){
+        return "templates/estateIndex";
+    }
+
+    @GetMapping("/estate-list")
+    public String getEstateList(@RequestParam("estate") Estate estate){
+        return "templates/estate";
+    }
+
 
 }
--- src/main/java/com/example/realestateaccounting/controller/WorkController.java
+++ src/main/java/com/example/realestateaccounting/controller/WorkController.java
@@ -1,12 +1,18 @@
 package com.example.realestateaccounting.controller;
 
 import com.example.realestateaccounting.dto.mapper.WorkMapper;
+import com.example.realestateaccounting.model.Estate;
+import com.example.realestateaccounting.model.Work;
 import com.example.realestateaccounting.service.WorkService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/work")
@@ -15,4 +21,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class WorkController {
     WorkService workService;
     WorkMapper workMapper;
+
+    @GetMapping("/index")
+    public String getWorkIndex(@RequestParam("workList") List<Work> workList){
+        return "workIndex";
+    }
+
+    @GetMapping("/list")
+    public String getWork(@RequestParam("estate")Estate estate){
+        return "work";
+    }
+
 }
