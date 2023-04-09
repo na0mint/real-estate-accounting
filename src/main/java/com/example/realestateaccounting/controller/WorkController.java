599d929fe315cbe040cbeff55d43fe3ceaa7df3a
NIkolay <kchucanon@gmail.com>
2023-04-09 16:30:47 +0300

    Controllers were extended + bug fixes

--- src/main/java/com/example/realestateaccounting/controller/WorkController.java
+++ src/main/java/com/example/realestateaccounting/controller/WorkController.java
@@ -5,10 +5,10 @@ import com.example.realestateaccounting.service.WorkService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/work")
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
